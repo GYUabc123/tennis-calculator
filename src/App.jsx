6546816949ab26ca392ba7184223a1f7070a5d13
Tyler <tylermauch18@gmail.com>
2023-09-12 16:30:37 -0400

    reset main and app css

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import BlackSquare from './component/BlackSquare'
+import NavBar from './component/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <div>
+        <NavBar/>
         <BlackSquare/>
       </div>
       
