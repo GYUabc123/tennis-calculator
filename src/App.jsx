225f6b655ba9277022ed641a57178dcf2e96d6e8
Tyler <tylermauch18@gmail.com>
2023-09-12 17:52:32 -0400

    navbar improved

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <>
       <div>
         <Calculator/>
-        <NavBar/>
+        <NavBar currentPage = "Home"/>
         <BlackSquare/>
       </div>
     </>
--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import "./NavBar.css"
-function NavBar() {
+function NavBar(props) {
+    console.log(props.currentPage)
   return (
     <div id = "navBar">
         <div id = "navBarHeader">Tennis Now</div>
+        <ul id = "navBarRightItems">
+            <li className = {props.currentPage == "Home" ? "navBarCurrentPage":"navBarListItem"}><a>Home</a></li>
+            <li className = "navBarListItem"><a>Login</a></li>
+        </ul>
     </div>
   )
 }
