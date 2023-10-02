adbb18a6b1a8082104dc86e19c2a16b1e69ec5b6
Gavin Yu <2003jyu@gmail.com>
2023-10-02 00:18:02 -0400

    added equation

--- src/screen/Calculator.jsx
+++ src/screen/Calculator.jsx
@@ -5,10 +5,11 @@ const netHeigh= .914; //in meter
 
 
  function Calculator() { //rfce
-    
+    const [Velocity, setVelocity] = useState(0);
+
     const [YPosition, setYPosition] = useState(0);
     const [YAngle, setXangle] = useState(0);
-    const [YVelocity, setVelocity] = useState(0);
+    const [YVelocity, setYVelocity] = useState(0);
     const [YAcceleration, setAcceleration] = useState(0);
 
     const [XAngle, setXAngle] = useState(0);
@@ -18,9 +19,14 @@ const netHeigh= .914; //in meter
     const [Time, setTime] = useState(0);
     const [TimeAtNet, setTimeAtNet] = useState(0);
 
+
    return (
      <div>calculator</div>
-     //jeljfsdjfklsjflksdjfljLF
+     //Calculation
+
+     //YVelocity = cos(YAngle) * Velocity
+     //0 = 4.9t^2 + YVelocityt - YPosition
+     // Time = (-1) * YVelocity + Math.sqrt(Math.pow(YVelocity, 2) - 4* 4.9 * (-1)*YPosition) 
 
 
    )
