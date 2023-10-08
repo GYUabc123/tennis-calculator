c7a99fa027f3ed02c3d35635b8f7425ca88643e2
Tyler <tylermauch18@gmail.com>
2023-10-08 12:32:17 -0400

    renamed home component and implemented react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './Home.jsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route index element = {<App/>}/>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
