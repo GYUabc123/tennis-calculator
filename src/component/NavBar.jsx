5ff9ef75f08eaa9c418f1127b91437c0b5061dd0
Tyler <tylermauch18@gmail.com>
2023-10-08 13:09:41 -0400

    navbar and navigation stuff

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import "./NavBar.css"
+import { Link } from 'react-router-dom'
 function NavBar(props) {
   return (
     <div id = "navBar">
         <div id = "navBarHeader">Tennis Now</div>
         <ul id = "navBarRightItems">
+
+            <Link to="/" style={{ textDecoration: 'none'}}>
             <li className = {props.currentPage == "Home" ? "navBarCurrentPage":"navBarListItem"}><a>Home</a></li>
-            <li className = "navBarListItem"><a>Login</a></li>
+            </Link>
+            <Link to="/login" style={{ textDecoration: 'none'}}>
+              <li className = {props.currentPage == "Login" ? "navBarCurrentPage":"navBarListItem"}>
+                <a>Login</a></li>
             <li className = "navBarListItem"><a>Sign Up</a></li>
+            </Link>
+
         </ul>
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './Home.jsx'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './component/Login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     <Routes>
       <Route index element = {<App/>}/>
+      <Route path = "/login" element={<Login/>}/>
     </Routes>
     </BrowserRouter>
   </React.StrictMode>,
