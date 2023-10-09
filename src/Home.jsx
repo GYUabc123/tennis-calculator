ad842dc772a0651060dc7248ce1e008a555f3a45
Gavin Yu <2003jyu@gmail.com>
2023-10-09 00:14:41 -0400

    add userInput

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './component/NavBar'
-import Calculator from './component/calculator'
+import Calculator from './component/Calculator'
 
 function App() {
   const [count, setCount] = useState(0)
