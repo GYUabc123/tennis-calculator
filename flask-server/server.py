0e1d6549d581b6328e3b17c2e94f9d664336769c
Tyler <tylermauch18@gmail.com>
2023-10-12 12:27:59 -0400

    changed stuff in flask server

--- flask-server/server.py
+++ flask-server/server.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "This is the home page"
+
 @app.route("/test")
 def test():
     return {"Testing":"Im testing"}
